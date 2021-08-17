202ba0655326520acc7bfe8496885ad326a9d279
t-gandhi-19 <89021506+t-gandhi-19@users.noreply.github.com>
2021-08-17 21:51:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def initialise(MEM):
 def getInstruction(PC, MEM):
     return MEM[PC]
 
+def flagReset(RF):
+    RF[7] = "0000000000000000"
+
 def main():
     MEM = ["0000000000000000"]*256
     RF = ["0000000000000000"]*8
@@ -21,9 +24,11 @@ def main():
     initialise(MEM)
     halted = False
 
-def execute(Instruction, RF):
-     def execute(Instruction, RF):
-    if(Instruction[0:5]=="00000" and Instruction[0:5]=="00001" and Instruction[0:5]=="000110" and Instruction[0:5]=="001010" and Instruction[0:5]== "001011" and Instruction[0:5]=="01100"):
+def execute(Instruction, RF, MEM, PC):
+
+    #Type A
+    if(Instruction[0:5]=="00000" or Instruction[0:5]=="00001" or Instruction[0:5]=="000110" or Instruction[0:5]=="001010" or Instruction[0:5]== "001011" or Instruction[0:5]=="01100"):
+        flagReset(RF)
         a=int(Instruction[7:10] , 2) 
         b =int(Instruction[10:13] , 2) 
         c= int(Instruction[13:16], 2)
@@ -62,36 +67,94 @@ def execute(Instruction, RF):
             s= str(bin(d))
             val = s[2:].zfill(16)
             RF[a] = val
-       
+
+    #Type B
     if (Instruction[0:5] == "00010" or Instruction[0:5] == "01000" or Instruction[0:5] == "01001"):
+        flagReset(RF)
         a = int(Instruction[5:8], 2)
         b = int(Instruction[8:], 2)
         if (Instruction[0:5] == "00010"):
             val = Instruction[8:].zfill(16)
             RF[a] = val
+            return False, PC + 1
+
         elif(Instruction[0:5] == "01000"):
             val = RF[a]
             intVal = int(val, 2)
             finVal = intVal >> b
             finVal = (bin(finVal)[2:]).zfill(16)
             RF[a] = finVal
+            return False, PC + 1
+
         elif(Instruction[0:5] == "01001"):
             val = RF[a]
             intVal = int(val, 2)
             finVal = intVal << b
             finVal = (bin(finVal)[2:]).zfill(16)
             RF[a] = finVal
-            
-    if (Instruction[0:5]=="00100" or Instruction[0:5]=="00101" ):     
+            return False, PC + 1
+
+
+    #Type C
+    if(Instruction[0:5] == "00011" or Instruction[0:5] == "00111" or Instruction[0:5] == "01101" or Instruction[0:5] == "01110"):
+        
+        a = int(Instruction[10:13], 2)
+        b = int(Instruction[13:16], 2)
+        if(Instruction[0:5] == "00011"):
+            val = RF[b]
+            RF[a] = val
+            flagReset(RF)
+            return False, PC + 1
+
+        if(Instruction[0:5] == "00111"):
+            flagReset(RF)
+            op1 = int(RF[a], 2)
+            op2 = int(RF[b], 2)
+            q = int(op1/op2)
+            r = int(op1%op2)
+            RF[0] = bin(q)[2:].zfill(16)
+            RF[1] = bin(r)[2:].zfill(16)
+            #overflow
+            return False, PC + 1
+
+        if(Instruction[0:5] == "01101"):
+            flagReset(RF)
+            op2 = RF[b]
+            inverse = ""
+            for i in op2:
+                if i == "0":
+                    inverse += "1"
+                else:
+                    inverse += "0"
+            RF[a] = inverse
+            return False, PC + 1
+
+        if(Instruction[0:5] == "01110"):
+            flagReset(RF)
+            op1 = int(RF[a], 2)
+            op2 = int(RF[b], 2)
+            if(op1<op2):
+                RF[7] = "0000000000000100"
+            elif(op1>op2):
+                RF[7] = "0000000000000010"
+            elif(op1 == op2):
+                RF[7] = "0000000000000001"
+            return False, PC + 1
+
+
+    #Type D
+    if (Instruction[0:5]=="00100" or Instruction[0:5]=="00101" ):
+        flagReset(RF)     
         a=int(Instruction[5:8] ,2)
         b=int(Instruction[8:], 2)
         if(Instruction[0:5]=="00100"): 
             RF[a]= MEM[b]
+            return False, PC + 1
 
         if(Instruction[0:5]=="00101"):
             MEM[b]=RF[a]
-    
-   
-      
+            return False, PC + 1
+
+
 
 
