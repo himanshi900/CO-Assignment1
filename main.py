dbbe7a2fbc499d00d073a9376b439806034fa38c
t-gandhi-19 <89021506+t-gandhi-19@users.noreply.github.com>
2021-08-17 20:07:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ def getInstruction(PC, MEM):
 
 def main():
     MEM = ["0000000000000000"]*256
+    RF = ["0000000000000000"]*8
+    PC = 0
+    initialise(MEM)
+    halted = False
+
+def execute(Instruction, RF):
+    if (Instruction[0:5] == "00010" or Instruction[0:5] == "01000" or Instruction[0:5] == "01001"):
+        a = int(Instruction[5:8], 2)
+        b = int(Instruction[8:], 2)
+        if (Instruction[0:5] == "00010"):
+            val = Instruction[8:].zfill(16)
+            RF[a] = val
+
+
