fb7265c686650815447e143aa03338f3db711746
Himanshi Sethi <40739078+himanshi900@users.noreply.github.com>
2021-08-18 22:53:44 +0530

    Update sim.py

--- sim.py
+++ sim.py
@@ -1,4 +1,7 @@
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 import sys
 import os
 from sys import stdin
@@ -32,7 +35,7 @@ def printMEM(MEM):
     for i in range(256):
         print(MEM[i].rstrip())
 
-def execute(Instruction, RF, MEM, PC):
+def execute(Instruction, RF, MEM, PC, mem_add):
 
     #Type A
     if(Instruction[0:5]=="00000" or Instruction[0:5]=="00001" or Instruction[0:5]=="000110" or Instruction[0:5]=="001010" or Instruction[0:5]== "001011" or Instruction[0:5]=="01100"):
@@ -48,12 +51,12 @@ def execute(Instruction, RF, MEM, PC):
                 l = len(s) - 16
                 val = s[l:].zfill(16)
                 RF[a] = val
-                return False, PC + 1 
+                return False, PC + 1 , PC
             else:
                 s= str(bin(d))
                 val = s[2:].zfill(16)
                 RF[a] = val
-                return False, PC + 1
+                return False, PC + 1 , PC
         #overflow
 
         if(Instruction[0:5]=="00001"):
@@ -62,27 +65,27 @@ def execute(Instruction, RF, MEM, PC):
                 RF[7]="0000000000001000"
                 s= "0000000000000000" 
                 RF[a] = s 
-                return False, PC + 1
+                return False, PC + 1 , PC
             else:
                 s= str(bin(d))         #-ve subtraction
                 val = s[2:].zfill(16)
                 RF[a] = val
-                return False, PC + 1
+                return False, PC + 1 , PC
 
         if(Instruction[0:5]=="00110"):
-            d= int(RF[b],2) * int(RF[c],2) #overflow check
+            d= int(RF[b],2) * int(RF[c],2) #overflow
             if(d>65535):
                 RF[7] == "0000000000001000"
                 s= str(bin(d))[2:]
                 l = len(s) - 16
                 val = s[l:].zfill(16)
                 RF[a] = val
-                return False, PC + 1 
+                return False, PC + 1 , PC
             else:
                 s= str(bin(d))
                 val = s[2:].zfill(16)
                 RF[a] = val
-                return False, PC + 1 
+                return False, PC + 1  , PC
 
 
         if(Instruction[0:5]=="001010"):    # xor
@@ -90,21 +93,21 @@ def execute(Instruction, RF, MEM, PC):
             s= str(bin(d))
             val = s[2:].zfill(16)
             RF[a] = val
-            return False, PC + 1
+            return False, PC + 1 , PC
 
         if(Instruction[0:5]=="001100"):   # and 
             d= int(RF[b],2) & int(RF[c],2) #overflow
             s= str(bin(d))
             val = s[2:].zfill(16)
             RF[a] = val
-            return False, PC + 1
+            return False, PC + 1, PC
         
         if(Instruction[0:5]=="001011"): # or
             d= int(RF[b],2) | int(RF[c],2) 
             s= str(bin(d))
             val = s[2:].zfill(16)
             RF[a] = val
-            return False, PC + 1
+            return False, PC + 1, PC
 
     #Type B
     if (Instruction[0:5] == "00010" or Instruction[0:5] == "01000" or Instruction[0:5] == "01001"):
@@ -114,7 +117,7 @@ def execute(Instruction, RF, MEM, PC):
         if (Instruction[0:5] == "00010"):
             val = bin(b)[2:].zfill(16)
             RF[a] = val
-            return False, PC + 1
+            return False, PC + 1 , PC
 
         elif(Instruction[0:5] == "01000"):
             val = RF[a]
@@ -122,7 +125,7 @@ def execute(Instruction, RF, MEM, PC):
             finVal = intVal >> b
             finVal = (bin(finVal)[2:]).zfill(16)
             RF[a] = finVal
-            return False, PC + 1
+            return False, PC + 1 , PC
 
         elif(Instruction[0:5] == "01001"):
             val = RF[a]
@@ -130,7 +133,7 @@ def execute(Instruction, RF, MEM, PC):
             finVal = intVal << b
             finVal = (bin(finVal)[2:]).zfill(16)
             RF[a] = finVal
-            return False, PC + 1
+            return False, PC + 1 , PC
 
 
     #Type C
@@ -142,7 +145,7 @@ def execute(Instruction, RF, MEM, PC):
             val = RF[b]
             RF[a] = val
             flagReset(RF)
-            return False, PC + 1
+            return False, PC + 1 , PC
 
         if(Instruction[0:5] == "00111"):
             flagReset(RF)
@@ -156,7 +159,7 @@ def execute(Instruction, RF, MEM, PC):
             RF[0] = bin(q)[2:].zfill(16)
             RF[1] = bin(r)[2:].zfill(16)
             
-            return False, PC + 1
+            return False, PC + 1 , PC
 
         if(Instruction[0:5] == "01101"):
             flagReset(RF)
@@ -168,7 +171,7 @@ def execute(Instruction, RF, MEM, PC):
                 else:
                     inverse += "0"
             RF[a] = inverse
-            return False, PC + 1
+            return False, PC + 1 , PC
 
         if(Instruction[0:5] == "01110"):
             flagReset(RF)
@@ -180,7 +183,7 @@ def execute(Instruction, RF, MEM, PC):
                 RF[7] = "0000000000000010"
             elif(op1 == op2):
                 RF[7] = "0000000000000001"
-            return False, PC + 1
+            return False, PC + 1 , PC
 
 
     #Type D
@@ -190,69 +193,92 @@ def execute(Instruction, RF, MEM, PC):
         b=int(Instruction[8:], 2)
         if(Instruction[0:5]=="00100"): 
             RF[a]= MEM[b]
-            return False, PC + 1
+            mem_add = b
+            return False, PC + 1 , mem_add
 
         if(Instruction[0:5]=="00101"):
             MEM[b]=RF[a]
-            return False, PC + 1
+            mem_add = b
+            return False, PC + 1, mem_add
     #Type E
     if(Instruction[0:5]=="01111" or Instruction[0:5]=="10000" or  Instruction[0:5]=="10001" or Instruction[0:5]=="10010"):
         a = int(Instruction[8:], 2)
         if(Instruction[0:5]=="01111"):
             flagReset(RF)
-            return False, a
+            return False, a , PC
         
         elif(Instruction[0:5]=="10000"):
             if(RF[7][-3]== "1"):
                 flagReset(RF)
-                return False, a
+                return False, a , PC
             else:
                 flagReset(RF)
-                return False, PC + 1
+                return False, PC + 1 , PC
 
     
         elif(Instruction[0:5]=="10001"):
             if(RF[7][-2]=="1"):
                 flagReset(RF)
-                return False, a
+                return False, a, PC
             else:
                 flagReset(RF)
-                return False, PC + 1
+                return False, PC + 1, PC
 
 
         elif(Instruction[0:5]=="10010"):
             if(RF[7][-1]=="1"):
                 flagReset(RF)
-                return False, a
+                return False, a, PC
             else:
                 flagReset(RF)
-                return False, PC + 1 
+                return False, PC + 1, PC 
     
     #Type F
     if(Instruction[0:5] == "10011"):
         flagReset(RF)
-        return True, PC + 1
+        return True, PC + 1, PC
+
         
 def main():
     MEM = ["0000000000000000"]*256
     RF = ["0000000000000000"]*8
     PC = 0
+    mem_add = 0
+    count=0
     initialise(MEM)
     halted = False
     while(not halted):
+        
         Instruction = getInstruction(PC, MEM) # Get current instruction
-        halted, new_PC = execute(Instruction, RF, MEM, PC) # Update RF compute new_PC
+        count+=1
+        mem_add = PC
+        
+        plt.scatter(count, PC)
+        
+        halted, new_PC, mem_add = execute(Instruction, RF, MEM, PC, mem_add) # Update RF compute new_PC
+        
+        
+        if(mem_add != PC):
+            plt.scatter(count,mem_add)
         a = printPC(PC)
         b = printRF(RF)
         print(a + " " + b)
         PC = new_PC
     printMEM(MEM)
+    
+    
+    plt.xlabel("cycle number")
+    plt.ylabel("memory address")
+    plt.show()
+    plt.savefig("matplotlib.png")
+
+
+
 
 main()        
             
-            
-
-        
+          
+    
 
 
 
